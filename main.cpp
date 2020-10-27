5f0d9b2716330a9f7e63fd830ad9eb84c9786a41
Marcin Litka <marcin.litka@gmail.com>
2020-10-27 23:44:19 +0100

    Second task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!";
+    std::cout << "Hello World!"<< std::endl;
+    std::cout << "This is second task!"<< std::endl;
     return 0;
 }
